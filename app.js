63aca48797e6907ee48c3bf97c371cb2b33e2bec
Jakub <jakub-klejczyk@o2.pl>
2022-08-01 19:11:44 +0200

    task without babel

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const del = document.querySelector("#del");
 const num = document.querySelector("#number");
 const basket = document.querySelector("#basket");
 const div = document.querySelector("#div");
+const ppp = document.querySelector("#ppp");
 
-let productsInBasket = 0;
+class Product {
+  constructor(name, price, number) {
+    this.name = name;
+    this.price = price;
+    this.number = number;
+    this.totalPrice = price * number;
+  }
+  addItem() {
+    this.number++;
+    num.textContent = this.number;
+  }
+  removeItem() {
+    this.number--;
+    num.textContent = this.number;
+  }
+}
+
+const vipera = new Product("Vipera kaznakovi", 5, 0);
+const { name, price, number, totalPrice } = vipera;
+ppp.textContent = price;
 
 btn.addEventListener("click", () => {
-  productsInBasket++;
-  num.textContent = productsInBasket;
+  vipera.addItem();
 });
 
 del.addEventListener("click", () => {
   if (productsInBasket > 0) {
-    productsInBasket--;
-    num.textContent = productsInBasket;
+    vipera.removeItem();
   }
 });
 
